0b8feda85f68500b72ed5ada567f6fe03469968b
Yeji Ha <yeji206000@gmail.com>
2024-04-17 15:01:53 -0700

    add the PhotoListItem component and display photo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,25 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <h2>{sampleDataForPhotoListItem.username}</h2>
+      <img src={sampleDataForPhotoListItem.profile} alt="Profile" />
+      <p>Location: {sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}</p>
+      <img src={sampleDataForPhotoListItem.imageSource} alt="Scenic view in Montreal" />
     </div>
   );
 };
