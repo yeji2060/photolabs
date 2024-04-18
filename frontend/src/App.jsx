10b1e13d86b82b780e360174d7781356ea318cb8
Yeji Ha <yeji206000@gmail.com>
2024-04-18 00:36:56 -0700

    display 3 photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,12 +13,14 @@ const sampleDataForPhotoListItem = {
   username: "Joe Example",
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
+const photos = new Array(3).fill(sampleDataForPhotoListItem);
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem photo={sampleDataForPhotoListItem} />
+      {photos.map((photo, index) => (
+        <PhotoListItem key={index} photo={photo} />
+      ))}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -11,7 +11,9 @@ const PhotoListItem = (props) => {
       <h2>{props.photo.username}</h2>
       {/* Apply the user profile and image styles */}
       <img src={props.photo.profile} alt="Profile" className="photo-list__user-profile" />
-      <p>Location: {props.photo.location.city}, {props.photo.location.country}</p>
+      <div className="photo-list__user-location">
+        <p>Location: {props.photo.location.city}, {props.photo.location.country}</p>
+      </div>
       <img src={props.photo.imageSource} alt="Scenic view in Montreal" className="photo-list__image" />
     </div>
   );
