ed7354b04f5981e5e3d3608335ca0a4091dc1359
Yeji Ha <yeji206000@gmail.com>
2024-04-18 00:05:42 -0700

    add styling to the PhotoListItem using Sass

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,13 +6,16 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <div>
+    // Add the specific class name to ensure styles are applied
+    <div className="photo-list__item">
       <h2>{props.photo.username}</h2>
-      <img src={props.photo.profile} alt="Profile" />
+      {/* Apply the user profile and image styles */}
+      <img src={props.photo.profile} alt="Profile" className="photo-list__user-profile" />
       <p>Location: {props.photo.location.city}, {props.photo.location.country}</p>
-      <img src={props.photo.imageSource} alt="Scenic view in Montreal" />
+      <img src={props.photo.imageSource} alt="Scenic view in Montreal" className="photo-list__image" />
     </div>
   );
 };
+
 
 export default PhotoListItem;
