36f78373ff492344a18d48ad105a4e0c47a5f3a8
Yeji Ha <yeji206000@gmail.com>
2024-04-19 06:07:44 -0700

    display mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,30 +1,15 @@
 import React from "react";
+import TopicListItem from "./TopicListItem";
+import topics from "mocks/topics";
 
 import "../styles/TopicList.scss";
-import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map(topic => (
+      {topics.map(topic => (
         <li key={topic.id}>
           <TopicListItem topic={{
             id: topic.id,
