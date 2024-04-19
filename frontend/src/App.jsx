8a6da65907a326f32371bb63664ccf3fe0ff3a5d
Yeji Ha <yeji206000@gmail.com>
2024-04-19 05:58:02 -0700

    build TopNavigationBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
  import PhotoList from './components/PhotoList';
- import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 
  import './App.scss';
 
 
  const App = () => (
    <div className="App">
-    <TopicList/>
+    <TopNavigation />
     <PhotoList/>
    </div>
  )
--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
+
 
 const TopicListItem = (props) => {
   return (
