b4016171013e3aa01b474b1ce4bfda2dcbeb065c
Yeji Ha <yeji206000@gmail.com>
2024-04-29 01:18:35 +0900

    use hooks to HomeRoute

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -2,43 +2,24 @@ import React, { useCallback, useState } from 'react';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 import PhotoDetailsModal from './PhotoDetailsModal';
+import useApplicationData from 'hooks/useApplicationData';
 
 
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = (props) => {
-   // State to track if the photo is favorited or not
-   const [favoritePhotos, setFavoritePhotos] = useState(new Set());
-   const [isModalOpen, setModalOpen] = useState(false);
-   const [selectedPhoto, setSelectedPhoto] = useState(null);
-
-   const toggleFavorite = photoId => {
-    setFavoritePhotos(prev => {
-      const newFavs = new Set(prev);
-      if (newFavs.has(photoId)) {
-        newFavs.delete(photoId);
-      } else {
-        newFavs.add(photoId);
-      }
-      return newFavs;
-    });
-  };
-
-  const toggleModal = () => {
-    setModalOpen(prev => !prev);
-  };
-
-  const onPhotoClick = photoDetails => {
-    setSelectedPhoto(photoDetails); // Set the selected photo details
-    toggleModal(); // Open the modal
-  };
-
+  const {
+    state,
+    updateToFavPhotoIds,
+    setPhotoSelected,
+    onClosePhotoDetailsModal
+  } = useApplicationData();
 
   return (
     <div className="home-route">
-      <TopNavigation topics={props.topics} favoriteCount={favoritePhotos.size}/>
-      <PhotoList photos={props.photos} toggleFavorite={toggleFavorite} favoritePhotos={favoritePhotos} onPhotoClick={onPhotoClick}/>
-      {isModalOpen && <PhotoDetailsModal onClose={toggleModal} photo={selectedPhoto} toggleFavorite={toggleFavorite} favoritePhotos={favoritePhotos}/>}
+      <TopNavigation topics={props.topics} favoriteCount={state.favoritePhotos.size}/>
+      <PhotoList photos={props.photos} toggleFavorite={updateToFavPhotoIds} favoritePhotos={state.favoritePhotos} onPhotoClick={setPhotoSelected}/>
+      {state.isModalOpen && <PhotoDetailsModal onClose={onClosePhotoDetailsModal} photo={state.selectedPhoto} toggleFavorite={updateToFavPhotoIds} favoritePhotos={state.favoritePhotos}/>}
     </div>
   );
 };
